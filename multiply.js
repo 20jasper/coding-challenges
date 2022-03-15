c4d7b192ea39e62a0cd5d5ffecf8e2bc49ed6e59
Jacob Asper <jacobasper191@gmail.com>
2022-03-15 19:24:13 -0400

    added new codewars problem 3/15/22

---
+++ multiply.js
@@ -0,0 +1,16 @@
+//url https://www.codewars.com/kata/50654ddff44f800200000004
+//problem: This code does not execute properly. Try to figure out why.
+
+//code given
+// function multiply(a, b){
+//     a * b
+//    }
+
+//solution
+//The expression is evaluated, but it is never sent anywhere, so a return needs to be added
+function multiply(a, b){
+    return a * b;
+   }
+   
+
+   
