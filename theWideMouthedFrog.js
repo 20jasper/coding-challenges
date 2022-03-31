56e8e945acf802d34591746f47fbd1030db6f98a
Jacob Asper <jacobasper191@gmail.com>
2022-03-30 23:01:09 -0400

    The Wide-Mouthed frog!

---
+++ theWideMouthedFrog.js
@@ -0,0 +1,30 @@
+//URL--
+// https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+
+//INSTRUCTIONS--
+/* The wide-mouth frog is particularly interested in the eating habits of other creatures.
+
+He just can't stop asking the creatures he encounters what they like to eat. But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+When he meets the alligator, it then makes a tiny mouth.
+
+Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide
+
+*/
+
+//SOLUTION--
+/* I am going to check if the animal is an alligator and return small if so
+If not, return wide
+
+I forgot to make it case insensitive, so I changed it to check if the lowercase version of the animal is 'alligator'
+*/
+function mouthSize(animal) {
+	if (animal.toLowerCase() === 'alligator') {
+		return 'small';
+	}
+	return 'wide';
+}
+//TESTCASES--
+console.log(mouthSize('toucan')); //wide
+console.log(mouthSize('ant bear')); //wide
+console.log(mouthSize('alligator')); //small
