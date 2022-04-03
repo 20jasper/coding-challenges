6825d5a41cfc5fe9fe486a956338878b3549367d
Jacob Asper <jacobasper191@gmail.com>
2022-04-03 14:52:53 -0400

    functions 3 multiplying two numbers

---
+++ function3MultiplyingTwoNumbers.js
@@ -0,0 +1,16 @@
+//URL--
+// https://www.codewars.com/kata/523b66342d0c301ae400003b
+
+//INSTRUCTIONS--
+/* Implement a function which multiplies two numbers.
+*/
+
+//SOLUTION--
+/* I am going to make a function that takes two arguments 
+Then I will multiply the numbers together and return the value
+*/
+function multiply(number1, number2) {
+    return number1 * number2
+}
+//TESTCASES--
+console.log((multiply(2, 3))); //6
