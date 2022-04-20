6f2c175d883dd74cb254ac039ac374eb74f72f94
Jacob Asper <jacobasper191@gmail.com>
2022-04-20 01:34:13 -0400

    growth of a population

---
+++ growthOfAPopulation.js
@@ -0,0 +1,57 @@
+//URL--
+// https://www.codewars.com/kata/563b662a59afc2b5120000c6/javascript
+
+//INSTRUCTIONS--
+/* In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+At the end of the first year there will be: 
+1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+
+At the end of the 2nd year there will be: 
+1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+
+At the end of the 3rd year there will be:
+1141 + 1141 * 0.02 + 50 => 1213
+
+It will need 3 entire years.
+
+More generally given parameters:
+
+p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+
+the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+Examples:
+nb_year(1500, 5, 100, 5000) -> 15
+nb_year(1500000, 2.5, 10000, 2000000) -> 10
+
+Note:
+
+Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+
+*/
+
+//SOLUTION--
+/* I am going to set the years to 0 then increment it by one for each calculation
+I will multiply the current population by the percentage then add the new inhabitants
+Then I will round the number down to the nearest whole number
+I will run the calculation on the result while the population is less than the threshhold 
+Then I will return the number of years
+*/
+function nbYear(p0, percent, aug, p) {
+    let years = 0
+    let population = Math.floor(p0)
+    while (p > population) {
+        years++
+        population += population * percent / 100 + aug
+        population = Math.floor(population)
+    }
+    return years
+}
+//TESTCASES--
+console.log(nbYear(1500, 5, 100, 5000), 15);
+console.log(nbYear(1500000, 2.5, 10000, 2000000), 10);
+console.log(nbYear(1500000, 0.25, 1000, 2000000), 94);
+console.log(nbYear(2, 50, 1214, 94));
