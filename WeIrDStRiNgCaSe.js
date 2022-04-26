dbde3ba8719c5a404443bfef3ec361f15a1ac3a3
Jacob Asper <jacobasper191@gmail.com>
2022-04-26 18:17:30 -0400

    Refactored code

--- WeIrDStRiNgCaSe.js
+++ WeIrDStRiNgCaSe.js
@@ -15,21 +15,44 @@ toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg CaSe"
 //SOLUTION--
 /* I am going to split the string into an array by spaces and then use map to make all even indexes of each word lowercase and all odd indexes of each word uppercase
 Then I'll join the array into a string by spaces and return it
+
+After doing this, I realized it could be done with only one loop by subtracting one from the count whenever there is a space
 */
+
+// // old version
+// function toWeirdCase(string) {
+//     const arr = string.split(' ')
+//     const weirdCaseArr = arr.map(word => {
+//         let weirdString = ''
+//         for (let i = 0; i < word.length; i++) {
+//             if (i % 2 === 0) {
+//                 weirdString += word.charAt(i).toUpperCase()
+//             } else {
+//                 weirdString += word.charAt(i).toLowerCase()
+//             }
+//         }
+//         return weirdString
+//     })
+//     return weirdCaseArr.join(' ')
+// }
+
+//refactored version
 function toWeirdCase(string) {
-    const arr = string.split(' ')
-    const weirdCaseArr = arr.map(word => {
-        let weirdString = ''
-        for (let i = 0; i < word.length; i++) {
-            if (i % 2 === 0) {
-                weirdString += word.charAt(i).toUpperCase()
-            } else {
-                weirdString += word.charAt(i).toLowerCase()
-            }
+    const arr = [...string]
+    let weirdString = ''
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === ' ') {
+            count--
         }
-        return weirdString
-    })
-    return weirdCaseArr.join(' ')
+        if (i % 2 === 0) {
+            weirdString += arr[i].toUpperCase()
+        } else {
+            weirdString += arr[i].toLowerCase()
+        }
+        count++
+    }
+    return weirdString
 }
 //TESTCASES--
 console.log(toWeirdCase('This'), 'ThIs');
