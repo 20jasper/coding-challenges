77a26e24a10062056e6df49163d5b9b915fd0cc4
Jacob Asper <jacobasper191@gmail.com>
2022-05-01 21:15:29 -0400

    bouncing balls

---
+++ CodeWars/bouncingBalls.js
@@ -0,0 +1,68 @@
+//URL--
+// https://www.codewars.com/kata/5544c7a5cb454edb3c000047
+
+//INSTRUCTIONS--
+/* A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
+
+He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+
+His mother looks out of a window 1.5 meters from the ground.
+
+How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+Three conditions must be met for a valid experiment:
+
+    Float parameter "h" in meters must be greater than 0
+    Float parameter "bounce" must be greater than 0 and less than 1
+    Float parameter "window" must be less than h.
+
+If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+Note:
+
+The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+Examples:
+
+- h = 3, bounce = 0.66, window = 1.5, result is 3
+
+- h = 3, bounce = 1, window = 1.5, result is -1 
+
+(Condition 2) not fulfilled).
+
+
+*/
+
+//SOLUTION--
+/* I am going to check if h is greater than one, bounce is greater than 0 and less than 1, window is less than h
+If any of this conditions is false, return -1
+
+Set the counter to 1
+
+Until the h is less than window
+    increment the counter by 1
+    Multiply the height by the bounce
+    Increment the counter by if the height is more than the window
+return the count
+
+*/
+function bouncingBall(h, bounce, window) {
+    if (h > 1 && bounce > 0 && bounce < 1 && window < h) {
+        function nextBounce(h, bounce, window, count = 0) {
+            count++
+            h = h * bounce
+            if (h > window) {
+                count++
+                return nextBounce(h, bounce, window, count)
+            }
+            return count
+        }
+        return nextBounce(h, bounce, window)
+    }
+    return -1
+}
+//TESTCASES--
+console.log(bouncingBall(3.0, 1.0, 1.5), -1);
+console.log(bouncingBall(0, 0, 1.5), -1);
+console.log(bouncingBall(1, .5, 1.5), -1);
+console.log(bouncingBall(3, .5, 5), -1);
+console.log(bouncingBall(3.0, 1.0, 1.5), -1);
+console.log(bouncingBall(3.0, 0.66, 1.5), 3);
+console.log(bouncingBall(30.0, 0.66, 1.5), 15);
