d994a11f7366fa9a76830c82575235d2c11afb12
Jacob Asper <jacobasper191@gmail.com>
2022-05-09 11:10:32 -0400

    function 1 hello world

---
+++ CodeWars/function1HelloWorlds.js
@@ -0,0 +1,23 @@
+//URL--
+// https://www.codewars.com/kata/523b4ff7adca849afe000035
+
+//INSTRUCTIONS--
+/* Description:
+
+Make a simple function called greet that returns the most-famous "hello world!".
+
+Style Points
+
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+*/
+
+//SOLUTION--
+/* I am going to return "hello world!"
+*/
+function greet() {
+    return "hello world!"
+}
+//TESTCASES--
+console.log(typeof greet, "function");
+console.log(greet(), "hello world!");
