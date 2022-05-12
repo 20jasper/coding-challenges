f5515263e3d5956b9bec3740ed1946674eb9b414
Jacob Asper <jacobasper191@gmail.com>
2022-05-12 11:43:11 -0400

    pirates are the cannons ready

---
+++ CodeWars/piratesAreTheCannonsReady.js
@@ -0,0 +1,44 @@
+//URL--
+// https://www.codewars.com/kata/5748a883eb737cab000022a6
+
+//INSTRUCTIONS--
+/* Ahoy Matey!
+
+Welcome to the seven seas.
+
+You are the captain of a pirate ship.
+
+You are in battle against the royal navy.
+
+You have cannons at the ready.... or are they?
+
+Your task is to check if the gunners are loaded and ready, if they are: Fire!
+
+If they aren't ready: Shiver me timbers!
+
+Your gunners for each test case are 2, 3 or 4.
+
+When you check if they are ready their answers are in a dictionary and will either be: aye or nay
+
+Firing with less than all gunners ready is non-optimum (this is not fire at will, this is fire by the captain's orders or walk the plank, dirty sea-dog!)
+
+If all answers are 'aye' then Fire! if one or more are 'nay' then Shiver me timbers!
+*/
+
+//SOLUTION--
+/* I am going to get all the keys of the object and then check if each one's value is 'nay
+    if so return 'Shiver me timbers!'
+return 'Fire!'
+*/
+const cannonsReady = (gunners) => {
+    const keys = Object.keys(gunners)
+    for (let i = 0; i < keys.length; i++) {
+        if (gunners[keys[i]] === 'nay') {
+            return 'Shiver me timbers!'
+        }
+    }
+    return 'Fire!'
+}
+//TESTCASES--
+console.log(cannonsReady({ 'Mike': 'aye', 'Joe': 'aye', 'Johnson': 'aye', 'Peter': 'aye' }), 'Fire!');
+console.log(cannonsReady({ 'Mike': 'aye', 'Joe': 'nay', 'Johnson': 'aye', 'Peter': 'aye' }), 'Shiver me timbers!');
