a51a0247790aebf02190584452c209ab271c4f4f
Jacob Asper <jacobasper191@gmail.com>
2022-05-17 21:51:44 -0400

    nop

---
+++ CodeWars/NOP.js
@@ -0,0 +1,22 @@
+//URL--
+// https://www.codewars.com/kata/5842ceb013ee5048350000bd/
+
+//INSTRUCTIONS--
+/* 
+Please fix the broken NOP function 
+
+var NOP = function() {
+  return "NOP"
+}
+*/
+
+//SOLUTION--
+/* 
+I am going to remove everything from inside the curly braces
+*/
+var NOP = function () {
+
+}
+
+//TESTCASES--
+console.log(NOP(1, 1), undefined);
