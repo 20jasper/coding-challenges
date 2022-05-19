6a3637ca7cdd3541e945abdaa0e2b8f8e6555412
Jacob Asper <jacobasper191@gmail.com>
2022-05-19 00:25:50 -0400

    5 without numbers

---
+++ CodeWars/5WithoutNumbers.js
@@ -0,0 +1,19 @@
+//URL--
+// https://www.codewars.com/kata/59441520102eaa25260000bf/
+
+//INSTRUCTIONS--
+/* Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck
+*/
+
+//SOLUTION--
+/* I am going to return the length of a string that is 5 characters long
+*/
+function unusualFive() {
+    return 'sdfss'.length
+}
+//TESTCASES--
+console.log(unusualFive(), 5);
