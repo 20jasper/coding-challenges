d7c7aa99a8c8a080dfc52146e675a2f5d91f84e1
Jacob Asper <jacobasper191@gmail.com>
2022-06-04 02:01:36 -0400

    css hello world

---
+++ CodeWars/CSSHelloWorld.js
@@ -0,0 +1,23 @@
+//URL--
+// https://www.codewars.com/kata/5c3bbc108a4c2e7531477a9a/
+
+//INSTRUCTIONS--
+/* Classic hello world created in CSS.
+
+Given the following html:
+
+<p>Hello</p>
+
+Modify the HTML trough CSS so it can print the classic phrase Hello world!
+
+IMPORTANT NOTE: since this code is based on Javascript you should end your CSS code with `
+
+*/ 
+
+//SOLUTION--
+/* I am going to use the after pseudoelement with the content property to add the text " world!" after the hello 
+*/ 
+// p:after{
+//     content: " world!";
+//   }
+//   `
