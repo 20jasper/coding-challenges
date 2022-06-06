a1fdb1161a64057b32092a83d0a5244a8d7a0f8f
Jacob Asper <jacobasper191@gmail.com>
2022-06-06 02:33:07 -0400

    make upper case

---
+++ CodeWars/makeUpperCase.js
@@ -0,0 +1,15 @@
+//URL--
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+//INSTRUCTIONS--
+/* Write a function which converts the input string to uppercase.
+*/
+
+//SOLUTION--
+/* return the input with the method toUpperCase at the end
+*/
+function makeUpperCase(str) {
+    return str.toUpperCase()
+}
+//TESTCASES--
+console.log(makeUpperCase("hello"), "HELLO");
