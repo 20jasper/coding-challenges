1c51e18b82fbf96bc9696f20fe3225511718f82c
Jacob Asper <jacobasper191@gmail.com>
2022-06-17 22:35:10 -0400

    you can't code under pressure

---
+++ CodeWars/youCantCodeUnderPressure1.js
@@ -0,0 +1,15 @@
+//URL--
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+
+//INSTRUCTIONS--
+/* Code as fast as you can! You need to double the integer and return it.
+*/
+
+//SOLUTION--
+/* I'm going to return 2 * the number
+*/
+function doubleInteger(i) {
+    return 2 * i;
+}
+//TESTCASES--
+console.log(doubleInteger(2), 4);
