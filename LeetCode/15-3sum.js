208dd0104baf2e391834ad6b711952ce1cb36fcd
Jacob Asper <jacobasper191@gmail.com>
2022-08-12 18:50:27 -0400

    renamed variables and started second solution

--- LeetCode/15-3sum.js
+++ LeetCode/15-3sum.js
@@ -38,6 +38,8 @@ Constraints:
 The first solution that comes to mind is using a triply nested for loop and just check if they all add up to 0
 if so, add them to a set so no triplets are repeated
 This solution is O(n^3) for time and O(1) for space where n is the length of the array
+
+This solution ended up timing out
 */
 /**
  * @param {number[]} nums
@@ -45,17 +47,17 @@ This solution is O(n^3) for time and O(1) for space where n is the length of the
  */
 const threeSum = function (nums) {
 	const set = new Set()
-	// iterate through the array from 3 points
+	// iterate through the array with three pointers that never overlap 
 	for (let i = 0; i < nums.length - 2; i++) {
 		for (let j = i + 1; j < nums.length - 1; j++) {
 			for (let k = j + 1; k < nums.length; k++) {
-				const num1 = nums[i];
-				const num2 = nums[j];
-				const num3 = nums[k];
+				const left = nums[i];
+				const mid = nums[j];
+				const right = nums[k];
 				//if the 3 nums add up to 0
-				if (num1 + num2 + num3 === 0) {
-					const tripletHash = [num1, num2, num3]
-						// sort so the hash is the same every time
+				if (left + mid + right === 0) {
+					const tripletHash = [left, mid, right]
+						// sort so the hash is the same for each unique combination
 						.sort((a, z) => a - z)
 						// join by a non number character to separate the numbers
 						.join("|")
@@ -77,3 +79,24 @@ const threeSum = function (nums) {
 console.log(threeSum([0, 1, 1]), []);
 console.log(threeSum([-1, 0, 1, 2, -1, -4]), [[-1, -1, 2], [-1, 0, 1]]);
 console.log(threeSum([0, 0, 0]), [[0, 0, 0]]);
+
+//SOLUTION--
+/* 
+This time, I will sort the array first
+When iterating through the array, if the next value for a pointer is the same as the current, skip it
+*/
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const threeSum2 = function (nums) {
+	const res = []
+	nums.sort((a, z) => a - z)
+
+	return nums
+};
+//TESTCASES--
+console.log(threeSum2([0, 1, 1]), []);
+console.log(threeSum2([-4, -1, -1, 0, 1, 2]), [[-1, -1, 2], [-1, 0, 1]]);
+console.log(threeSum2([0, 0, 0]), [[0, 0, 0]]);
+console.log(threeSum2([0, 0, 0, 0, 0]), [[0, 0, 0]]);
