b07861432ae21ba8ebbbe5786f4652ff60f5d6d2
Jacob Asper <jacobasper191@gmail.com>
2022-09-06 17:28:27 -0400

    added test cases

--- LeetCode/239-slidingWindowMaximum.js
+++ LeetCode/239-slidingWindowMaximum.js
@@ -42,4 +42,5 @@ const maxSlidingWindow = function (nums, k) {
 
 };
 //TESTCASES--
-console.log();
+console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3), [3, 3, 5, 5, 6, 7]);
+console.log(maxSlidingWindow([1], 1), [1]);
