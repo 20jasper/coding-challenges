9fbb0b6930487606fd8c0299fd0031a17cf80c65
Jacob Asper <jacobasper191@gmail.com>
2022-10-11 23:57:32 -0400

    add more explanation

--- LeetCode/560-subarraySumEqualsK.js
+++ LeetCode/560-subarraySumEqualsK.js
@@ -56,6 +56,9 @@ console.log(subarraySum([0, 1, -1], 0), 3);
 //SOLUTION--
 /* 
 The running sum at any index i - the running sum at another index j shows gives us the running sum from i to j
+
+For example for array `[-1, 1, 2, 3, 6]`, the running sums at each index would be `[-1, 0, 2, 5, 11]`, so if we wanted to get the sum of the numbers between the 2nd and 4th index, we can just subtract 11-2 and get 9, and checking back in the original array, 3+6 is indeed 9.
+
 So to solve this problem, we need to find each pair i and j where i-j =k and each time a running sum is k
 This solution has a space complexity of O(n) and a time complexity of O(n) where n is the length of the array
 */
@@ -82,3 +85,4 @@ console.log(subarraySum2([1, 1, 1], 2), 2);
 console.log(subarraySum2([1, 2, 3], 3), 2);
 console.log(subarraySum2([0, 1, -1], 0), 3);
 console.log(subarraySum2([-2, 2, 0, 4, 5], 4), 3);
+console.log(subarraySum2([-1, 1, 2, 3, 6], 6), 2);
