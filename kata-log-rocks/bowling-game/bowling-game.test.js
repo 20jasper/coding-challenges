08690a3cb0cb667d3bc15f8aa83a419ed97c8666
Jacob Asper <jacobasper191@gmail.com>
2022-11-22 20:09:41 -0500

    add line breaks between frames

--- kata-log-rocks/bowling-game/bowling-game.test.js
+++ kata-log-rocks/bowling-game/bowling-game.test.js
@@ -15,38 +15,47 @@ describe('bowling game', ()=>{
 	})
 	test('should add points for each pin knocked down', ()=>{
 		name.roll(5)
+		
 		expect(name._points).toBe(5)
 	})
 	test('The score method should return the score', ()=>{
 		name.roll(5)
+
 		expect(name.score()).toBe(5)
 	})
 	test('The frame should advance when all 10 pins are knocked down', ()=>{
 		name.roll(10)
+
 		expect(name._frame).toBe(1)
 	})
 	test('The frame should advance when a spare is rolled', ()=>{
 		name.roll(5)
 		name.roll(5)
+
 		expect(name._frame).toBe(1)
 	})
 	test('The frame should advance after 2 rolls that don\'t knock down all the pins', ()=>{
 		name.roll(5)
 		name.roll(4)
+
 		expect(name._frame).toBe(1)
 	})
 	// The bonus for a spare is the number of pins knocked down by the next roll.
 	test('Bonus points should be added after a spare', ()=>{
 		name.roll(5)
 		name.roll(5)
+
 		name.roll(5)
+
 		expect(name._points).toBe(20)
 	})
 	test('Bonus points should not be added 2 rolls after a spare', ()=>{
 		name.roll(5)
 		name.roll(5)
+
 		name.roll(5)
 		name.roll(4)
+
 		expect(name._points).toBe(24)
 	})
 })
