bb8e9ef24a588ec0829cb9000ccac165a9472131
Jacob Asper <jacobasper191@gmail.com>
2022-11-22 19:25:44 -0500

    track pins left and advance frame when 0 are left

--- kata-log-rocks/bowling-game/bowling-game.js
+++ kata-log-rocks/bowling-game/bowling-game.js
@@ -12,11 +12,14 @@ class Name{
 	constructor(){
 		this._frame = 0
 		this._points = 0
+		this._pinsLeft = 10
 	}
 
 	roll(pinsKnockedDown){
 		this._points+=pinsKnockedDown
-		if(pinsKnockedDown===10){
+		this._pinsLeft-=pinsKnockedDown
+		
+		if(this._pinsLeft===0){
 			this._frame++
 		}
 	}
