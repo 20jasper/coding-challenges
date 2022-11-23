c6dd598de6fa969b7a407ceab1abc49639a9b191
Jacob Asper <jacobasper191@gmail.com>
2022-11-22 19:09:58 -0500

    add test for point initialization

--- kata-log-rocks/bowling-game/bowling-game.test.js
+++ kata-log-rocks/bowling-game/bowling-game.test.js
@@ -10,5 +10,9 @@ describe('bowling game', ()=>{
 	test('should initialize on the 0th frame', ()=>{
 		expect(name._frame).toBe(0)
 	})
+	test('should initialize with 0 points', ()=>{
+		expect(name._points).toBe(0)
+	})
+
 })
 
