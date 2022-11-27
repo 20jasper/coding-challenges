7fb792c163979005ceba0caf0c4be61815c39beb
Jacob Asper <jacobasper191@gmail.com>
2022-11-26 21:05:28 -0500

    add test for score initialization

--- kata-log-rocks/bowling-game-brian/bowling-game.test.js
+++ kata-log-rocks/bowling-game-brian/bowling-game.test.js
@@ -2,9 +2,19 @@ const { Game } = require('./bowling-game')
 
 
 describe('bowling game', ()=>{
+	let model
+
+	beforeEach(()=>{
+		model = new Game()
+	})
+
 	test('Game class should exist', ()=>{
-		const model = new Game()
 
 		expect(model).toBeDefined()
+	})
+
+	test('score is initialized as 0', ()=>{
+		
+		expect(model.score).toBe(0)
 	})
 })
