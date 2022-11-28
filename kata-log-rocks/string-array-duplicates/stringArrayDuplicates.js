b0ae63c170aaa22b864a17d21f628dcbf0084249
Jacob Asper <jacobasper191@gmail.com>
2022-11-28 17:39:06 -0500

    add boilerplate

---
+++ kata-log-rocks/string-array-duplicates/stringArrayDuplicates.js
@@ -0,0 +1,25 @@
+//URL--
+// https://www.codewars.com/kata/59f08f89a5e129c543000069
+
+//INSTRUCTIONS--
+/* 
+You will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+For example:
+
+dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+dup(["kelless","keenness"]) = ["keles","kenes"].
+Strings will be lowercase only, no spaces 
+
+dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars','picaniny','hubububo'])
+dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese'])
+dup(["kelless","keenness"]), ['keles','kenes'])
+*/ 
+
+//SOLUTION--
+/* 
+Take in array of strings
+return array of strings with duplicate chars in each string removed
+Strings are always lowercase letters only
+*/ 
+
