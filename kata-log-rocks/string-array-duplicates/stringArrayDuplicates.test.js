dad388488ad48c931f1978d4795bb1174bb69b59
Jacob Asper <jacobasper191@gmail.com>
2022-11-28 17:51:44 -0500

    add test for input and output length

--- kata-log-rocks/string-array-duplicates/stringArrayDuplicates.test.js
+++ kata-log-rocks/string-array-duplicates/stringArrayDuplicates.test.js
@@ -4,4 +4,10 @@ describe('String Array Duplicates', () => {
 	test('function should exist', () => {
 		expect(stringArrayDuplicates).toBeDefined()
 	});
+
+	test('length of input and output array should be the same', () => {
+		const data = []
+
+		expect(stringArrayDuplicates(data).length).toBe(0)
+	});
 });
