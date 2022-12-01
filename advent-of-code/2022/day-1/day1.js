56e26810774a9279b2b792498a56dc0429adcdd8
Jacob Asper <jacobasper191@gmail.com>
2022-12-01 17:12:01 -0500

    parse file with for loop

--- advent-of-code/2022/day-1/day1.js
+++ advent-of-code/2022/day-1/day1.js
@@ -14,12 +14,23 @@ function getMaxCalories(elves) {
 
 function parseInput(relativePath) {
 	const absolutePath = `${__dirname}/${relativePath}`
-
 	const data = fs.readFileSync(absolutePath, { encoding: 'utf8' })
 
 	const caloriesArr = data.split('\n')
 
-	return caloriesArr
+	const res = [[]]
+
+	for (let i = 0; i < caloriesArr.length; i++) {
+		// go to next elf if end of current elf's calories
+		if (caloriesArr[i] === '') {
+			res.push([])
+			continue
+		}
+
+		res.at(-1).push(Number(caloriesArr[i]))
+	}
+
+	return res
 }
 
 module.exports = { getMaxCalories, parseInput }
