7e5467719a372db4ea7a86947258042eca3d15bf
Jacob Asper <jacobasper191@gmail.com>
2022-12-01 13:19:31 -0500

    Nest arrays to better format data

--- advent-of-code/2022/day-1/day1.test.js
+++ advent-of-code/2022/day-1/day1.test.js
@@ -1,7 +1,9 @@
+const { getMaxCalories } = require('./day1')
+
 describe('Max Calories', () => {
 	it('should return the total of the first elf if there is only one', () => {
 
-		expect(10).toBe(getMaxCalories([10]))
-		expect(9).toBe(getMaxCalories([3, 3, 3]))
+		expect(10).toBe(getMaxCalories([[10]]))
+		expect(9).toBe(getMaxCalories([[3, 3, 3]]))
 	});
 });
