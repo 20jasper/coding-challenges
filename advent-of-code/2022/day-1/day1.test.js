428b96ff324b98af17d7a2e7524e846a1563b200
Jacob Asper <jacobasper191@gmail.com>
2022-12-01 13:41:14 -0500

    flipped received and expected data in tests

--- advent-of-code/2022/day-1/day1.test.js
+++ advent-of-code/2022/day-1/day1.test.js
@@ -3,12 +3,12 @@ const { getMaxCalories } = require('./day1')
 describe('Max Calories', () => {
 	it('should return the total of the first elf if there is only one', () => {
 
-		expect(10).toBe(getMaxCalories([[10]]))
-		expect(9).toBe(getMaxCalories([[3, 3, 3]]))
+		expect(getMaxCalories([[10]])).toBe(10)
+		expect(getMaxCalories([[3, 3, 3]])).toBe(9)
 	});
 
 	it('should return the second elf\'s total if it has more calories', () => {
 
-		expect(11).toBe(getMaxCalories([[10], [11]]))
+		expect(getMaxCalories([[10], [11]])).toBe(11)
 	});
 });
