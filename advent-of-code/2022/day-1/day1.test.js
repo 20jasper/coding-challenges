5dab925a0a6d4af01d156a2a725e27afef285ecd
Jacob Asper <jacobasper191@gmail.com>
2022-12-01 13:17:02 -0500

    add test for if there's one elf

---
+++ advent-of-code/2022/day-1/day1.test.js
@@ -0,0 +1,7 @@
+describe('Max Calories', () => {
+	it('should return the total of the first elf if there is only one', () => {
+
+		expect(10).toBe(getMaxCalories([10]))
+		expect(9).toBe(getMaxCalories(3, 3, 3))
+	});
+});
