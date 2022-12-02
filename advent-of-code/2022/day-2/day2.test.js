69c0fcbecd0e6ba6d9a881a4329a353ea62a20cf
Jacob Asper <jacobasper191@gmail.com>
2022-12-02 12:19:40 -0500

    remove comments

--- advent-of-code/2022/day-2/day2.test.js
+++ advent-of-code/2022/day-2/day2.test.js
@@ -8,18 +8,13 @@ const { rockPaperScissors } = require('./day2')
 
 describe('Rock paper Scissors', () => {
 	it('should award points for shape you selected', () => {
-		//opponent: paper, you: rock
 		expect(rockPaperScissors([['paper', 'rock']])).toBe(1)
-		//opponent: scissors, you: paper
 		expect(rockPaperScissors([['scissors', 'paper']])).toBe(2)
-		//opponent: rock, you: scissors
 		expect(rockPaperScissors([['rock', 'scissors']])).toBe(3)
 	});
 
 	it('should add points based on round outcome', () => {
-		//opponent: rock, you: paper => win
 		expect(rockPaperScissors([['rock', 'paper']])).toBe(8)
-		//oponent: rock, you: rock => tie
 		expect(rockPaperScissors([['rock', 'rock']])).toBe(4)
 	});
 });
