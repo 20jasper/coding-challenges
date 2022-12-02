13ff49c1793ae3e8757d0078098c4410139f6a9e
Jacob Asper <jacobasper191@gmail.com>
2022-12-02 16:07:41 -0500

    refactor tests to accept win/tie/lose in tuple

--- advent-of-code/2022/day-2/day2Part2.test.js
+++ advent-of-code/2022/day-2/day2Part2.test.js
@@ -1,33 +1,33 @@
 // cases
-// change X/Y/Z to win/tie/loss
+// change X/Y/Z to lose/tie/win
 
 const { rockPaperScissors, parseInput } = require('./day2Part2')
 
 
 describe('Rock paper Scissors', () => {
 	it('should award points for shape you selected', () => {
-		expect(rockPaperScissors([['paper', 'rock']])).toBe(1)
-		expect(rockPaperScissors([['scissors', 'paper']])).toBe(2)
-		expect(rockPaperScissors([['rock', 'scissors']])).toBe(3)
+		expect(rockPaperScissors([['paper', 'lose']])).toBe(1)
+		expect(rockPaperScissors([['scissors', 'lose']])).toBe(2)
+		expect(rockPaperScissors([['rock', 'lose']])).toBe(3)
 	});
 
 	it('should add points on tie', () => {
-		expect(rockPaperScissors([['rock', 'rock']])).toBe(4)
+		expect(rockPaperScissors([['rock', 'tie']])).toBe(4)
 	});
 
 	it('should add points based on win', () => {
-		expect(rockPaperScissors([['rock', 'paper']])).toBe(8)
-		expect(rockPaperScissors([['paper', 'scissors']])).toBe(9)
-		expect(rockPaperScissors([['scissors', 'rock']])).toBe(7)
+		expect(rockPaperScissors([['rock', 'win']])).toBe(8)
+		expect(rockPaperScissors([['paper', 'win']])).toBe(9)
+		expect(rockPaperScissors([['scissors', 'win']])).toBe(7)
 	});
 
 	it('should sum multiple rounds', () => {
-		expect(rockPaperScissors([['rock', 'paper'], ['rock', 'paper']])).toBe(16)
+		expect(rockPaperScissors([['rock', 'win'], ['rock', 'win']])).toBe(16)
 	});
 });
 
 describe('parse input', () => {
 	it('should change each letter to rock, paper, or scissors', () => {
-		expect(parseInput("test.txt")).toStrictEqual([['rock', 'rock'], ['paper', 'paper'], ['scissors', 'scissors']])
+		expect(parseInput("test.txt")).toStrictEqual([['rock', 'lose'], ['paper', 'tie'], ['scissors', 'win']])
 	});
 });
