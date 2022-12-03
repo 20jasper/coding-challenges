176ac488cc3f109c4e1f29b5f0b7827f0165871c
Jacob Asper <jacobasper191@gmail.com>
2022-12-03 16:51:53 -0500

    add to TODO

--- advent-of-code/2022/day-3/day3Part1.test.js
+++ advent-of-code/2022/day-3/day3Part1.test.js
@@ -2,6 +2,9 @@
 // find the duplicate letter in each half of the rucksack
 // get the priority of that letter
 // sum all the priorities of each rucksack
+// change alphabet string to hashmap
+// get duplicate letter by adding each letter to a set
+
 const { getPrioritySum } = require('./day3Part1')
 
 describe('Get priority sum', () => {
