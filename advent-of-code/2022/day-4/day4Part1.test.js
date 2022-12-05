7d41ed71c48644eddf9fff98117ac75f9a5f20da
Jacob Asper <jacobasper191@gmail.com>
2022-12-04 22:34:04 -0500

    add test for if section is fully contained

--- advent-of-code/2022/day-4/day4Part1.test.js
+++ advent-of-code/2022/day-4/day4Part1.test.js
@@ -2,3 +2,12 @@
 // Parse input
 // Find fully contained section assignments
 // Get sum of all fully contained assignments
+
+const { getFullyContainedSum } = require('./day4Part1')
+
+describe('Get fully contained sum', () => {
+
+	it('should add to the section if fully contained', () => {
+		expect(getFullyContainedSum([[[1, 2], [1, 2]]])).toBe(1)
+	});
+});
