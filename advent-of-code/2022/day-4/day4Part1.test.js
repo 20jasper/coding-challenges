51f9e8ccabdc0237408c00dd13bc3a7f80f8ff7e
Jacob Asper <jacobasper191@gmail.com>
2022-12-05 00:14:20 -0500

    change comments into their own tests

--- advent-of-code/2022/day-4/day4Part1.test.js
+++ advent-of-code/2022/day-4/day4Part1.test.js
@@ -7,21 +7,26 @@ const { getFullyContainedSum, parseInput } = require('./day4Part1')
 
 describe('Get fully contained sum', () => {
 
-	it('should add to the section if fully contained', () => {
-		// if starts are equal
-		expect(getFullyContainedSum([[[1, 2], [1, 3]]])).toBe(1)
-		// if ends are equal
-		expect(getFullyContainedSum([[[2, 2], [1, 2]]])).toBe(1)
-		// if start and end are in between
-		expect(getFullyContainedSum([[[2, 2], [1, 3]]])).toBe(1)
-		expect(getFullyContainedSum([[[1, 3], [2, 2]]])).toBe(1)
+	describe('should add to the section if fully contained', () => {
+		it('starts are equal', () => {
+			expect(getFullyContainedSum([[[1, 2], [1, 3]]])).toBe(1)
+		});
+		it('ends are equal', () => {
+			expect(getFullyContainedSum([[[2, 2], [1, 2]]])).toBe(1)
+		});
+		it('start and end are in between', () => {
+			expect(getFullyContainedSum([[[2, 2], [1, 3]]])).toBe(1)
+			expect(getFullyContainedSum([[[1, 3], [2, 2]]])).toBe(1)
+		});
 	});
 
-	it('should not add to the section if not fully contained', () => {
-		// no overlap
-		expect(getFullyContainedSum([[[1, 2], [3, 4]]])).toBe(0)
-		// 2nd overlaps partially
-		expect(getFullyContainedSum([[[2, 4], [3, 5]]])).toBe(0)
+	describe('should not add to the section if not fully contained', () => {
+		it('no overlap between sections ', () => {
+			expect(getFullyContainedSum([[[1, 2], [3, 4]]])).toBe(0)
+		});
+		it('2nd function overlaps, but isn\'t fully contained', () => {
+			expect(getFullyContainedSum([[[2, 4], [3, 5]]])).toBe(0)
+		});
 	});
 
 	it('should sum multiple pairs', () => {
