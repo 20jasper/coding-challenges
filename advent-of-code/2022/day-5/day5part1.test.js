8c89a43fbef5392b8612d4c9593df3cee9ecae47
Jacob Asper <jacobasper191@gmail.com>
2022-12-05 16:51:33 -0500

    add test for multiple operations and refactor other tests

--- advent-of-code/2022/day-5/day5part1.test.js
+++ advent-of-code/2022/day-5/day5part1.test.js
@@ -11,19 +11,23 @@ describe('rearrangeStacks', () => {
 	describe('should get correct stack tops', () => {
 
 		it('if stack is one long', () => {
-			expect(rearrangeStacks([['C'], ['G']])).toBe('CG')
-			expect(rearrangeStacks([['C'], ['G'], ['V']])).toBe('CGV')
+			expect(rearrangeStacks([['C'], ['G']], [])).toBe('CG')
+			expect(rearrangeStacks([['C'], ['G'], ['V']], [])).toBe('CGV')
 		});
 
 		it('if stacks are longer than 1', () => {
-			expect(rearrangeStacks([['C', 'A'], ['G', 'C', 'Q']])).toBe('AQ')
+			expect(rearrangeStacks([['C', 'A'], ['G', 'C', 'Q']], [])).toBe('AQ')
 		});
 	});
 
-	describe.only('Should move from one stack to another', () => {
-		const stacks = [['A', 'B', 'C'], ['D', 'E', 'F']]
+	it('Should move from one stack to another', () => {
+		expect(rearrangeStacks([['A', 'B'], ['D']], [{ amount: 1, start: 1, end: 2 }])).toBe('AB')
+		expect(rearrangeStacks([['A'], ['D', 'E', 'F']], [{ amount: 2, start: 2, end: 1 }])).toBe('ED')
+	});
 
-		expect(rearrangeStacks(stacks, { amount: 1, start: 1, end: 2 })).toBe('BC')
-		expect(rearrangeStacks(stacks, { amount: 2, start: 2, end: 1 })).toBe('FE')
+	it('should do multiple operations', () => {
+		const stacks = [['A', 'B', 'C'], ['D', 'E']]
+
+		expect(rearrangeStacks(stacks, [{ amount: 1, start: 1, end: 2 }, { amount: 2, start: 2, end: 1 }])).toBe('ED')
 	});
 });
