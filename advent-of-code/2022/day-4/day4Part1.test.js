665cf3b033a48a1f469d81d4b7e286a2d8b7601d
Jacob Asper <jacobasper191@gmail.com>
2022-12-04 22:42:39 -0500

    add test for if section is not fully contained

--- advent-of-code/2022/day-4/day4Part1.test.js
+++ advent-of-code/2022/day-4/day4Part1.test.js
@@ -10,4 +10,8 @@ describe('Get fully contained sum', () => {
 	it('should add to the section if fully contained', () => {
 		expect(getFullyContainedSum([[[1, 2], [1, 2]]])).toBe(1)
 	});
+
+	it('should not add to the section if not fully contained', () => {
+		expect(getFullyContainedSum([[[1, 2], [3, 4]]])).toBe(0)
+	});
 });
