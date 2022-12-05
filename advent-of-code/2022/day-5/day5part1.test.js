3c3093f8317bf04b03409011aaae2d834b350152
Jacob Asper <jacobasper191@gmail.com>
2022-12-05 12:23:42 -0500

    add tests for getting stack tops

--- advent-of-code/2022/day-5/day5part1.test.js
+++ advent-of-code/2022/day-5/day5part1.test.js
@@ -4,3 +4,19 @@
 // move from one column to another
 // get top of crate string
 // do multiple operations
+
+const { rearrangeStacks } = require('./day5part1');
+describe('rearrangeStacks', () => {
+
+	describe('should get correct stack tops', () => {
+
+		it('if stack is one long', () => {
+			expect(rearrangeStacks([['C'], ['G']])).toBe('CG')
+			expect(rearrangeStacks([['C'], ['G'], ['V']])).toBe('CGV')
+		});
+
+		it('if stacks are longer than 1', () => {
+			expect(rearrangeStacks([['C', 'A'], ['G', 'C', 'Q']])).toBe('AQ')
+		});
+	});
+});
