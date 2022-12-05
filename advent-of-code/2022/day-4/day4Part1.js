a9c83ea70733333f966f6df2436509a0702c93a3
Jacob Asper <jacobasper191@gmail.com>
2022-12-04 22:31:03 -0500

    renamed to part 1

--- advent-of-code/2022/day-4/day4Part1.js
+++ advent-of-code/2022/day-4/day4Part1.js
@@ -18,9 +18,9 @@ For example, consider the following list of section assignment pairs:
 
 For the first few pairs, this list means:
 
-    Within the first pair of Elves, the first Elf was assigned sections 2-4 (sections 2, 3, and 4), while the second Elf was assigned sections 6-8 (sections 6, 7, 8).
-    The Elves in the second pair were each assigned two sections.
-    The Elves in the third pair were each assigned three sections: one got sections 5, 6, and 7, while the other also got 7, plus 8 and 9.
+		Within the first pair of Elves, the first Elf was assigned sections 2-4 (sections 2, 3, and 4), while the second Elf was assigned sections 6-8 (sections 6, 7, 8).
+		The Elves in the second pair were each assigned two sections.
+		The Elves in the third pair were each assigned three sections: one got sections 5, 6, and 7, while the other also got 7, plus 8 and 9.
 
 This example list uses single-digit section IDs to make it easier to draw; your actual list might contain larger numbers. Visually, these pairs of section assignments look like this:
 
@@ -45,8 +45,13 @@ This example list uses single-digit section IDs to make it easier to draw; your 
 Some of the pairs have noticed that one of their assignments fully contains the other. For example, 2-8 fully contains 3-7, and 6-6 is fully contained by 4-6. In pairs where one assignment fully contains the other, one Elf in the pair would be exclusively cleaning sections their partner will already be cleaning, so these seem like the most in need of reconsideration. In this example, there are 2 such pairs.
 
 In how many assignment pairs does one range fully contain the other?
-*/ 
+*/
 
 //SOLUTION--
 /* 
-*/ 
+*/
+function getFullyContainedSum(pairs) {
+
+}
+
+module.exports = { getFullyContainedSum }
