05c32038bef4505142f04ee4b303c91277d47c69
Jacob Asper <jacobasper191@gmail.com>
2022-12-06 20:06:56 -0500

    add tests for if one to two numbers are passed in

--- kata-log-rocks/string-calculator/stringCalculator.test.js
+++ kata-log-rocks/string-calculator/stringCalculator.test.js
@@ -7,5 +7,14 @@ describe('String Calculator', () => {
 		it('should return 0 if given an empty string', () => {
 			expect(Add('')).toBe(0)
 		});
+
+		it('should return the number if only one number is passed in', () => {
+			expect(Add('5')).toBe(5)
+			expect(Add('1000')).toBe(1000)
+		});
+
+		it('should return the sum if two numbers are passed in', () => {
+			expect(Add('5,7')).toBe(12)
+		});
 	});
 });
