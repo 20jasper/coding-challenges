fd13c7a38bcd60a4dd6a1eb9b1439e08268c1fa8
Jacob Asper <jacobasper191@gmail.com>
2022-12-06 19:54:18 -0500

    add boilerplate

---
+++ kata-log-rocks/string-calculator/stringCalculator.js
@@ -0,0 +1,33 @@
+//URL--
+// https://osherove.com/tdd-kata-1/
+
+//INSTRUCTIONS--
+/*
+1. Create a simple String calculator with a method signature:
+
+———————————————
+
+int Add(string numbers)
+
+———————————————
+
+The method can take up to two numbers, separated by commas, and will return their sum.
+
+for example “” or “1” or “1,2” as inputs.
+
+(for an empty string it will return 0)
+
+Hints:
+
+——————
+
+ - Start with the simplest test case of an empty string and move to one and two numbers
+
+ - Remember to solve things as simply as possible so that you force yourself to write tests you did not think about
+
+ - Remember to refactor after each passing test
+*/
+
+//SOLUTION--
+/* 
+*/
