f2dd38e9ed51e219938a54b46983899cc0d29153
Jacob Asper <jacobasper191@gmail.com>
2023-02-13 02:00:52 -0500

    read from file

--- advent-of-code/2022/day-1/Rust/day_1_part_1/src/main.rs
+++ advent-of-code/2022/day-1/Rust/day_1_part_1/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+	let file_path = "inputt.txt";
+
+	let input = match fs::read_to_string(file_path) {
+		Ok(string) => string,
+		Err(e) => panic!("couldn't read {file_path}: {e}"),
+	};
+
+	println!("{input}");
 }
