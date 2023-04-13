df995148954a6b35f86cee147e67d730d5116474
Jacob Asper <jacobasper191@gmail.com>
2023-04-13 19:55:10 -0400

    read from file

--- advent-of-code/2022/day-1/Rust/day_1_part_1/src/main.rs
+++ advent-of-code/2022/day-1/Rust/day_1_part_1/src/main.rs
@@ -1,12 +1,28 @@
 use std::fs;
 
 fn main() {
-	let file_path = "inputt.txt";
+	let file_path = "input.txt";
 
-	let input = match fs::read_to_string(file_path) {
+	let input = read_file(file_path);
+	println!("{input}");
+}
+
+fn read_file(file_path: &str) -> String {
+	match fs::read_to_string(file_path) {
 		Ok(string) => string,
 		Err(e) => panic!("couldn't read {file_path}: {e}"),
-	};
+	}
+}
 
-	println!("{input}");
+#[cfg(test)]
+mod tests {
+	use crate::read_file;
+
+	#[test]
+	fn should_read_from_file() {
+		let expected_input = "2621\n4136\n";
+		let input = read_file("test_input.txt");
+
+		assert_eq!(input, expected_input)
+	}
 }
