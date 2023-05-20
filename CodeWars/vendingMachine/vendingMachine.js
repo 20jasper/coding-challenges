42b0db8a08e241cf147b269fc81a054ed2364948
Jacob Asper <jacobasper191@gmail.com>
2023-05-20 15:29:03 -0400

    not sure, but added some new stuff since git is not working

--- CodeWars/vendingMachine/vendingMachine.js
+++ CodeWars/vendingMachine/vendingMachine.js
@@ -1,29 +1,58 @@
 // @ts-check
 
 /**
- * @constructor
- * @param {{name: string, 
+ * @typedef {{name: string,
  * 					code: string,
  * 					quantity: number,
  * 					price: number
- * 				}[]} items All of the snacks to choose from in the machine
+ * 				}} item
+ */
+
+/**
+ * @constructor
+ * @param {item[]} items All of the snacks to choose from in the machine
  * @param {number} money the amount of change in the vending machine
  */
 function VendingMachine(items, money) {
-	this._items = items
-};
+  this._items = items;
+  this._money = money;
+}
+
+/**
+ * @param {string} selection the code entered into the machine
+ * @param {item[]} items All of the snacks to choose from in the machine
+ * @returns {item | undefined} the item selected or `undefined` if it doesn't exist
+ */
+const getItem = (selection, items) =>
+  items.find(({ code }) => code === selection);
 
 /**
  * @param {string} selection the code entered into the machine
  * @param {number} itemMoney the amount of money the customer put in the machine
  * @returns {string} the message returned by the machine
  */
-VendingMachine.prototype.vend = function (selection, itemMoney){
-	const [item] = this._items
-	if(item.price > itemMoney){
-		return "Not enough money!"
-	}
-	return `Vending ${item.name}.`
+VendingMachine.prototype.vend = function (selection, itemMoney) {
+  const item = getItem(selection, this._items);
+
+  if (item === undefined) {
+    return `Invalid selection! : Money in vending machine = ${this._money.toFixed(
+      2
+    )}`;
+  }
+
+  const { price, name, quantity } = item;
+
+  if (quantity === 0) {
+    return `${name}: Out of stock!`;
+  }
+
+  const change = itemMoney - price;
+  if (change < 0) {
+    return "Not enough money!";
+  } else if (change === 0) {
+    return `Vending ${name}.`;
+  }
+  return `Vending ${name} with ${change.toFixed(2)} change.`;
 };
 
-module.exports = VendingMachine
+module.exports = VendingMachine;
--- CodeWars/vendingMachine/vendingMachine.test.js
+++ CodeWars/vendingMachine/vendingMachine.test.js
@@ -1,25 +1,44 @@
 // @ts-check
-const VendingMachine = require('./vendingMachine')
+const VendingMachine = require("./vendingMachine");
 
-describe('Vending Machine', () => {
-	/** @type {Parameters<typeof VendingMachine>[0]} */
-	const items = [{
-		name: "beans",
-		code: "A1",
-		quantity: 1,
-		price: 1
-	}];
+describe("Vending Machine", () => {
+  /** @type {Parameters<typeof VendingMachine>[0]} */
+  const items = [
+    {
+      name: "beans",
+      code: "A1",
+      quantity: 1,
+      price: 1,
+    },
+    { name: "apple", code: "A2", quantity: 0, price: 1 },
+  ];
 
-	test('should return "not enough money!" if not enough money', () => {
-		const vendingMachine = new VendingMachine(items, 0)
+  test('should return "not enough money!" if not enough money', () => {
+    const vendingMachine = new VendingMachine(items, 0);
 
-		expect(vendingMachine.vend("A1", 0)).toBe("Not enough money!")
-	});
+    expect(vendingMachine.vend("A1", 0)).toBe("Not enough money!");
+  });
 
-	test('should return "Vending <name of item>" if exactly the correct change', () => {
-		const vendingMachine = new VendingMachine(items, 0)
-		const itemName = "beans"
+  test('should return "Vending <name of item>" if no change', () => {
+    const vendingMachine = new VendingMachine(items, 0);
+    const itemName = "beans";
 
-		expect(vendingMachine.vend("A1", 1)).toBe(`Vending ${itemName}.`)
-	});
+    expect(vendingMachine.vend("A1", 1)).toBe(`Vending ${itemName}.`);
+  });
+
+  test('should return "Vending <name of item> with <change> change." if positive change', () => {
+    const vendingMachine = new VendingMachine(items, 0);
+    const itemName = "beans";
+
+    expect(vendingMachine.vend("A1", 1.2)).toBe(
+      `Vending ${itemName} with 0.20 change.`
+    );
+  });
+
+  test('should return "<name of item> out of stock" if quantity is 0', () => {
+    const vendingMachine = new VendingMachine(items, 0);
+		const itemName = "apple";
+		
+    expect(vendingMachine.vend("A2", 1.2)).toBe(`${itemName}: Out of stock!`);
+  });
 });
