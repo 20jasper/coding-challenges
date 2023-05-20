8b1d589f0a4bf4402b09e4823fc436b253fa1256
Jacob Asper <jacobasper191@gmail.com>
2023-05-20 16:31:03 -0400

    refactor tests

--- CodeWars/vendingMachine/vendingMachine.test.js
+++ CodeWars/vendingMachine/vendingMachine.test.js
@@ -4,6 +4,8 @@ const VendingMachine = require("./vendingMachine");
 describe("Vending Machine", () => {
   /** @type {Parameters<typeof VendingMachine>[0]} */
   let items;
+  /** @type {VendingMachine}*/
+  let vendingMachine;
 
   beforeEach(() => {
     items = [
@@ -15,23 +17,20 @@ describe("Vending Machine", () => {
       },
       { name: "apple", code: "A2", quantity: 0, price: 1 },
     ];
+    vendingMachine = new VendingMachine(items, 100)
   });
 
   test('should return "not enough money!" if not enough money', () => {
-    const vendingMachine = new VendingMachine(items, 0);
-
     expect(vendingMachine.vend("A1", 0)).toBe("Not enough money!");
   });
 
   test('should return "Vending <name of item>" if no change', () => {
-    const vendingMachine = new VendingMachine(items, 0);
     const itemName = "beans";
 
     expect(vendingMachine.vend("A1", 1)).toBe(`Vending ${itemName}`);
   });
 
   test('should return "Vending <name of item> with <change> change." if positive change', () => {
-    const vendingMachine = new VendingMachine(items, 0);
     const itemName = "beans";
 
     expect(vendingMachine.vend("A1", 1.2)).toBe(
@@ -40,25 +39,18 @@ describe("Vending Machine", () => {
   });
 
   test('should return "<name of item> out of stock" if quantity is 0', () => {
-    const vendingMachine = new VendingMachine(items, 0);
     const itemName = "apple";
 
     expect(vendingMachine.vend("A2", 1.2)).toBe(`${itemName}: Out of stock!`);
   });
 
   test('should return "invalid selection" if item not in vending machine', () => {
-    const money = 0;
-    const vendingMachine = new VendingMachine(items, money);
-
     expect(vendingMachine.vend("code that doesn't exist", 0)).toBe(
-      `Invalid selection! : Money in vending machine = ${money.toFixed(2)}`
+      `Invalid selection! : Money in vending machine = ${vendingMachine._money.toFixed(2)}`
     );
   });
 
   test("should update quantity on valid selection", () => {
-    const money = 0;
-    const vendingMachine = new VendingMachine(items, money);
-
     const initialQuantity = vendingMachine._items[0].quantity;
     vendingMachine.vend("A1", 9999);
 
