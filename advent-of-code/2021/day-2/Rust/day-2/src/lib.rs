8d7cc84e75bc603d5e5eca3d7a76fb922cd44e73
Jacob Asper <jacobasper191@gmail.com>
2023-11-13 00:47:05 -0500

    works with down instruction

--- advent-of-code/2021/day-2/Rust/day-2/src/lib.rs
+++ advent-of-code/2021/day-2/Rust/day-2/src/lib.rs
@@ -1,11 +1,28 @@
-pub fn get_position(instructions: String) -> (i32, i32) {
-    let lines = instructions.lines().map(|line| line.split(' '));
+pub fn get_position(text: String) -> (i32, i32) {
+    text.lines()
+        .map(|line| line.split(' '))
+        .map(|mut line| {
+            if let (Some(direction), Some(magnitude)) = (line.next(), line.next()) {
+                (direction, magnitude.parse::<i32>().unwrap())
+            } else {
+                unreachable!()
+            }
+        })
+        .fold(
+            (0, 0),
+            |(horizontal, depth), (direction, magnitude)| match direction {
+                "down" => (horizontal, depth + magnitude),
+                _ => unreachable!(),
+            },
+        )
+}
 
-    for mut line in lines {
-        if let (Some(direction), Some(magnitude)) = (line.next(), line.next()) {
-            dbg!(&direction, &magnitude);
-        };
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn down_should_increase_depth() {
+        assert_eq!(get_position("down 5".to_owned()), (0, 5));
     }
-
-    (0, 0)
 }
