eedf08add96ca5a5f5c45f27b33858648ee8b59f
Jacob Asper <jacobasper191@gmail.com>
2023-11-12 23:50:24 -0500

    Refactor to change aim instead of depth with up and down

--- advent-of-code/2021/day-2/TypeScript/src/index.ts
+++ advent-of-code/2021/day-2/TypeScript/src/index.ts
@@ -6,6 +6,9 @@ import url from "url";
 // fix config 10:52 haha
 // fix ts compilation garbage 11:00
 // parse input 11:09
+// finish 11:49
+
+// total time 1:14
 
 function readFromFile(): string {
   const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
@@ -28,24 +31,24 @@ export function parseInput(text: string): Instruction[] {
     .map(([direction, magnitude]) => [direction, Number(magnitude)]);
 }
 
-type Position = [horizontal: number, depth: number];
+type Position = [horizontal: number, depth: number, aim: number];
 
 export function getPosition(instructions: Instruction[]): Position {
   return instructions.reduce(
-    ([horizontal, depth], [direction, magnitude]) => {
+    ([horizontal, depth, aim], [direction, magnitude]) => {
       if (direction === "up") {
-        return [horizontal, depth - magnitude];
+        return [horizontal, depth, aim - magnitude];
       }
       if (direction === "down") {
-        return [horizontal, depth + magnitude];
+        return [horizontal, depth, aim + magnitude];
       }
       if (direction === "forward") {
-        return [horizontal + magnitude, depth];
+        return [horizontal + magnitude, depth + aim * magnitude, aim];
       }
 
       throw new Error(`Invalid direction: ${direction}`);
     },
-    [0, 0]
+    [0, 0, 0]
   );
 }
 
--- advent-of-code/2021/day-2/TypeScript/tests/index.test.ts
+++ advent-of-code/2021/day-2/TypeScript/tests/index.test.ts
@@ -9,25 +9,37 @@ describe("hi", () => {
     ]);
   });
 
-  it("should be negative depth when going up", () => {
-    expect(getPosition([["up", 10]])).toStrictEqual([0, -10]);
+  it("should be negative aim when going up", () => {
+    expect(getPosition([["up", 10]])).toStrictEqual([0, 0, -10]);
   });
 
-  it("should be positive depth when going down", () => {
-    expect(getPosition([["down", 10]])).toStrictEqual([0, 10]);
+  it("should be positive aim when going down", () => {
+    expect(getPosition([["down", 10]])).toStrictEqual([0, 0, 10]);
   });
 
   it("should be positive horizontal when going forward", () => {
-    expect(getPosition([["forward", 10]])).toStrictEqual([10, 0]);
+    expect(getPosition([["forward", 10]])).toStrictEqual([10, 0, 0]);
   });
 
-  it("should work with multiple directions", () => {
+  it("should increase depth by aim * magnitude", () => {
+    expect(
+      getPosition([
+        ["down", 5],
+        ["forward", 8],
+      ])
+    ).toStrictEqual([8, 40, 5]);
+  });
+
+  it("should work with multiple instructions", () => {
     expect(
       getPosition([
         ["forward", 5],
-        ["up", 10],
         ["down", 5],
+        ["forward", 8],
+        ["up", 3],
+        ["down", 8],
+        ["forward", 2],
       ])
-    ).toStrictEqual([5, -5]);
+    ).toStrictEqual([15, 60, 10]);
   });
 });
