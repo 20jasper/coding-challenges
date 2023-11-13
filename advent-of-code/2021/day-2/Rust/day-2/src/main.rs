d97f6fe3fb8505ce9ca3709c4e682b5f25970680
Jacob Asper <jacobasper191@gmail.com>
2023-11-13 00:25:52 -0500

    read from file

--- advent-of-code/2021/day-2/Rust/day-2/src/main.rs
+++ advent-of-code/2021/day-2/Rust/day-2/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
+// start 12:10
+// read file (they are relative to src) 12:23
 fn main() {
-    println!("Hello, world!");
+    let instructions = fs::read_to_string("../../input.txt").unwrap();
+
+    dbg!(&instructions);
 }
