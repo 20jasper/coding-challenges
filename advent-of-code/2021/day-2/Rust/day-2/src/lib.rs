cbbb5cc119d293cdf9de3684270870f271cd150a
Jacob Asper <jacobasper191@gmail.com>
2023-11-13 00:53:13 -0500

    Add forward and up directions

--- advent-of-code/2021/day-2/Rust/day-2/src/lib.rs
+++ advent-of-code/2021/day-2/Rust/day-2/src/lib.rs
@@ -3,15 +3,22 @@ pub fn get_position(text: String) -> (i32, i32) {
         .map(|line| line.split(' '))
         .map(|mut line| {
             if let (Some(direction), Some(magnitude)) = (line.next(), line.next()) {
-                (direction, magnitude.parse::<i32>().unwrap())
+                (
+                    direction,
+                    magnitude
+                        .parse::<i32>()
+                        .expect("invalid input, {magnitude} is not a number"),
+                )
             } else {
-                unreachable!()
+                panic!("Invalid input! not enough words on a line");
             }
         })
         .fold(
             (0, 0),
             |(horizontal, depth), (direction, magnitude)| match direction {
                 "down" => (horizontal, depth + magnitude),
+                "up" => (horizontal, depth - magnitude),
+                "forward" => (horizontal + magnitude, depth),
                 _ => unreachable!(),
             },
         )
@@ -24,5 +31,15 @@ mod tests {
     #[test]
     fn down_should_increase_depth() {
         assert_eq!(get_position("down 5".to_owned()), (0, 5));
+    }
+
+    #[test]
+    fn up_should_decrease_depth() {
+        assert_eq!(get_position("up 5".to_owned()), (0, -5));
+    }
+
+    #[test]
+    fn forward_should_decrease_depth() {
+        assert_eq!(get_position("forward 5".to_owned()), (5, 0));
     }
 }
