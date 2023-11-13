ba9358ef7529416ed6a840a9881a93e8bb764234
Jacob Asper <jacobasper191@gmail.com>
2023-11-13 02:12:11 -0500

    Refactor to add error handling

--- advent-of-code/2021/day-2/Rust/day-2/src/lib.rs
+++ advent-of-code/2021/day-2/Rust/day-2/src/lib.rs
@@ -1,31 +1,45 @@
-fn parse_instructions(text: &str) -> Vec<(&str, i32)> {
-    text.lines()
-        .map(|line| line.split(' '))
-        .map(|mut line| {
-            if let (Some(direction), Some(magnitude)) = (line.next(), line.next()) {
-                (
-                    direction,
-                    magnitude
-                        .parse::<i32>()
-                        .expect("invalid input, {magnitude} is not a number"),
-                )
-            } else {
-                panic!("Invalid input! not enough words on a line");
-            }
-        })
-        .collect::<Vec<(&str, i32)>>()
+use anyhow::{anyhow, Context, Result};
+
+enum Instruction {
+    Down(i32),
+    Up(i32),
+    Forward(i32),
 }
 
-pub fn get_position(text: String) -> (i32, i32, i32) {
-    parse_instructions(&text).iter().fold(
+/// attempts to parse an instruction into a direction and magnitude
+fn try_parse_instruction(instruction: Option<(&str, &str)>) -> Result<Instruction> {
+    instruction.map_or(Err(anyhow!("error")), |(direction, magnitude)| {
+        let magnitude = magnitude
+            .parse::<i32>()
+            .context("Could not parse magnitude {magnitude}")?;
+
+        match direction {
+            "down" => Ok(Instruction::Down(magnitude)),
+            "up" => Ok(Instruction::Up(magnitude)),
+            "forward" => Ok(Instruction::Forward(magnitude)),
+            _ => Err(anyhow!("Invalid direction: {direction}")),
+        }
+    })
+}
+
+fn parse_instructions(text: &str) -> Result<Vec<Instruction>> {
+    text.lines()
+        .map(|line| line.split_once(' '))
+        .map(try_parse_instruction)
+        .collect::<Result<Vec<Instruction>>>()
+}
+
+pub fn get_position(text: String) -> Result<(i32, i32, i32)> {
+    Ok(parse_instructions(&text)?.iter().fold(
         (0, 0, 0),
-        |(horizontal, depth, aim), (direction, magnitude)| match *direction {
-            "down" => (horizontal, depth, aim + magnitude),
-            "up" => (horizontal, depth, aim - magnitude),
-            "forward" => (horizontal + magnitude, depth + aim * magnitude, aim),
-            _ => unreachable!(),
+        |(horizontal, depth, aim), instruction| match instruction {
+            Instruction::Down(magnitude) => (horizontal, depth, aim + magnitude),
+            Instruction::Up(magnitude) => (horizontal, depth, aim - magnitude),
+            Instruction::Forward(magnitude) => {
+                (horizontal + magnitude, depth + aim * magnitude, aim)
+            }
         },
-    )
+    ))
 }
 
 #[cfg(test)]
@@ -34,23 +48,23 @@ mod tests {
 
     #[test]
     fn down_should_increase_aim() {
-        assert_eq!(get_position("down 5".to_owned()), (0, 0, 5));
+        assert_eq!(get_position("down 5".to_owned()).unwrap(), (0, 0, 5));
     }
 
     #[test]
     fn up_should_decrease_aim() {
-        assert_eq!(get_position("up 5".to_owned()), (0, 0, -5));
+        assert_eq!(get_position("up 5".to_owned()).unwrap(), (0, 0, -5));
     }
 
     #[test]
     fn forward_should_increase_horizontal() {
-        assert_eq!(get_position("forward 5".to_owned()), (5, 0, 0));
+        assert_eq!(get_position("forward 5".to_owned()).unwrap(), (5, 0, 0));
     }
 
     #[test]
     fn forward_and_positive_aim_increases_depth() {
         assert_eq!(
-            get_position("forward 5\ndown 5\nforward 5".to_owned()),
+            get_position("forward 5\ndown 5\nforward 5".to_owned()).unwrap(),
             (10, 25, 5)
         );
     }
--- advent-of-code/2021/day-2/Rust/day-2/src/main.rs
+++ advent-of-code/2021/day-2/Rust/day-2/src/main.rs
@@ -2,6 +2,8 @@ use std::fs;
 
 use day_2::get_position;
 
+use anyhow::Result;
+
 // start 12:10
 // read file (they are relative to src) 12:23
 // split magnitude and direction 12:35
@@ -9,10 +11,12 @@ use day_2::get_position;
 // done part 2 12:58
 // refactor 1:06
 // total time 0:54
-fn main() {
-    let instructions = fs::read_to_string("../../input.txt").unwrap();
+fn main() -> Result<()> {
+    let instructions = fs::read_to_string("../../input.txt")?;
 
-    let (horizontal, depth, _) = get_position(instructions);
+    let (horizontal, depth, _) = get_position(instructions)?;
 
     println!("answer: {}", horizontal * depth);
+
+    Ok(())
 }
