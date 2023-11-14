0db79ac45bb4dd1e1b9bb618be9231abddfc3411
Jacob Asper <jacobasper191@gmail.com>
2023-11-13 20:01:50 -0500

    finish go

--- advent-of-code/2021/day-2/Go/cmd/day-2/main.go
+++ advent-of-code/2021/day-2/Go/cmd/day-2/main.go
@@ -3,23 +3,68 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 // start 7:18
 // reads file relative to go.mod 7:24
-func main() {
+// finish 8:01
+
+func readFileToString() (string, error) {
 	bytes, err := os.ReadFile("../input.txt")
 
 	if err != nil {
-		fmt.Print(err)
+		return "", err
 	}
 
-	str := string(bytes)
+	return string(bytes), nil
+}
 
-	lines := strings.Split(str, "\n")
+func getPosition(text string) (int, int, error) {
+	horizontal := 0
+	depth := 0
+
+	lines := strings.Split(text, "\n")
 
 	for _, line := range lines {
-		fmt.Println(line)
+		if line == "" {
+			continue
+		}
+
+		instruction := strings.Split(line, " ")
+		direction := instruction[0]
+
+		magnitude, err := strconv.Atoi(instruction[1])
+		if err != nil {
+			fmt.Println(line, "line is brokey")
+			return 0, 0, err
+		}
+
+		switch direction {
+		case "up":
+			depth -= magnitude
+		case "down":
+			depth += magnitude
+		case "forward":
+			horizontal += magnitude
+		}
 	}
+
+	return horizontal, depth, nil
+}
+
+func main() {
+	str, err := readFileToString()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	horizontal, depth, err := getPosition(str)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(horizontal * depth)
 }
