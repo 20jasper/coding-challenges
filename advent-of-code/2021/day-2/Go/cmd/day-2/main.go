c70ae67b80821740c63fc4e577b677d1e34cf645
Jacob Asper <jacobasper191@gmail.com>
2023-11-13 19:25:19 -0500

    Read from file go

--- advent-of-code/2021/day-2/Go/cmd/day-2/main.go
+++ advent-of-code/2021/day-2/Go/cmd/day-2/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+// start 7:18
+// reads file relative to go.mod 7:24
+func main() {
+	bytes, err := os.ReadFile("../input.txt")
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	str := string(bytes)
+
+	fmt.Println(str)
 }
