4803b1c59ad034d8b239273b3f57d87e656a4b0a
Jacob Asper <jacobasper191@gmail.com>
2023-12-01 19:07:44 -0500

    properly error handle solution

--- advent-of-code/2023/rust/day_1/src/lib.rs
+++ advent-of-code/2023/rust/day_1/src/lib.rs
@@ -1,26 +1,46 @@
-pub fn parse_calibration_value(line: &str) -> u64 {
-    let first_digit = line.chars().find(char::is_ascii_digit).unwrap();
-    let last_digit = line.chars().rfind(char::is_ascii_digit).unwrap();
+use anyhow::{Context, Result};
 
-    format!("{first_digit}{last_digit}").parse::<u64>().unwrap()
+pub fn parse_calibration_value((i, line): (usize, &str)) -> Result<u64> {
+    let format_line_err = || format!("could not parse digit in line {i}: {line}");
+
+    let first_digit = line
+        .chars()
+        .find(char::is_ascii_digit)
+        .with_context(format_line_err)?;
+    let last_digit = line
+        .chars()
+        .rfind(char::is_ascii_digit)
+        .with_context(format_line_err)?;
+
+    format!("{first_digit}{last_digit}")
+        .parse::<u64>()
+        .with_context(format_line_err)
 }
 
 #[cfg(test)]
 mod tests {
+    use anyhow::Ok;
+
     use super::*;
 
     #[test]
-    fn should_parse_two_digits() {
-        assert_eq!(parse_calibration_value("1abc2"), 12);
+    fn should_parse_two_digits() -> Result<()> {
+        assert_eq!(parse_calibration_value((0, "1abc2"))?, 12);
+
+        Ok(())
     }
 
     #[test]
-    fn should_repeat_if_only_one_digit() {
-        assert_eq!(parse_calibration_value("1ab"), 11);
+    fn should_repeat_if_only_one_digit() -> Result<()> {
+        assert_eq!(parse_calibration_value((0, "1ab"))?, 11);
+
+        Ok(())
     }
 
     #[test]
-    fn should_work_with_digits_in_middle() {
-        assert_eq!(parse_calibration_value("aaaaa89bbbbb"), 89);
+    fn should_work_with_digits_in_middle() -> Result<()> {
+        assert_eq!(parse_calibration_value((0, "aaaaa89bbbbb"))?, 89);
+
+        Ok(())
     }
 }
--- advent-of-code/2023/rust/day_1/src/main.rs
+++ advent-of-code/2023/rust/day_1/src/main.rs
@@ -1,9 +1,16 @@
+use anyhow::{Ok, Result};
 use day_1::parse_calibration_value;
 
-fn main() {
+fn main() -> Result<()> {
     let file = include_str!("../input.txt");
 
-    let sum: u64 = file.lines().map(parse_calibration_value).sum();
+    let sum = file
+        .lines()
+        .enumerate()
+        .map(parse_calibration_value)
+        .try_fold(0, |acc, val| Ok(val? + acc))?;
 
     println!("{}", sum);
+
+    Ok(())
 }
