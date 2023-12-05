4c57a80e55eae96f8e47a4775de7d63c57e67c4c
Jacob Asper <jacobasper191@gmail.com>
2023-12-05 04:01:42 -0500

    parse lines and seeds

--- advent-of-code/2023/rust/day-5/src/part1.rs
+++ advent-of-code/2023/rust/day-5/src/part1.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 use itertools::Itertools;
 
 use crate::custom_error::AocError;
@@ -8,7 +10,8 @@ pub fn process(_input: &str) -> miette::Result<String, AocError> {
 
     let (seeds, maps) = group_seeds_and_maps(file);
 
-    dbg!(seeds);
+    let seeds = parse_seeds(seeds);
+    let maps = maps.iter().map(|map| parse_map(map));
 
     for map in maps {
         println!();
@@ -19,6 +22,48 @@ pub fn process(_input: &str) -> miette::Result<String, AocError> {
     }
 
     todo!()
+}
+
+#[derive(Debug)]
+struct Line {
+    range: Range<i64>,
+    /// source to destination
+    difference: i64,
+    source_start: i64,
+    destination_start: i64,
+    range_length: i64,
+}
+
+fn parse_map_line(line: &str) -> Line {
+    let mut vals = line
+        .split_whitespace()
+        .map(|x| x.parse::<i64>().unwrap());
+
+    let (destination_start, source_start, range_length) = (
+        vals.next().unwrap(),
+        vals.next().unwrap(),
+        vals.next().unwrap(),
+    );
+
+    Line {
+        range: (source_start..(source_start + range_length)),
+        difference: source_start - destination_start,
+        source_start,
+        destination_start,
+        range_length,
+    }
+}
+
+fn parse_map<'a>(map: &'a [&'a str]) -> impl Iterator<Item = Line> + 'a {
+    map.iter()
+        .map(|line: &&str| parse_map_line(line))
+}
+
+fn parse_seeds(seeds: &str) -> impl Iterator<Item = u64> + '_ {
+    seeds
+        .trim_start_matches("seeds: ")
+        .split_whitespace()
+        .map(|seed| seed.parse::<u64>().unwrap())
 }
 
 fn group_seeds_and_maps(file: &str) -> (&str, Vec<Vec<&str>>) {
