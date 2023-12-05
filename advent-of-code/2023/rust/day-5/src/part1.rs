f489f7815371a04f843ea78d1b4f9c4edd26fbe2
Jacob Asper <jacobasper191@gmail.com>
2023-12-05 03:08:03 -0500

    group seeds and maps

--- advent-of-code/2023/rust/day-5/src/part1.rs
+++ advent-of-code/2023/rust/day-5/src/part1.rs
@@ -1,10 +1,53 @@
+use itertools::Itertools;
+
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(_input: &str) -> miette::Result<String, AocError> {
+    let file = include_str!("../input1.txt");
+
+    let (seeds, maps) = group_seeds_and_maps(file);
+
+    dbg!(seeds);
+
+    for map in maps {
+        println!();
+        println!("NEW MAP");
+        for line in map {
+            dbg!(line);
+        }
+    }
+
+    todo!()
+}
+
+fn group_seeds_and_maps(file: &str) -> (&str, Vec<Vec<&str>>) {
+    // seeds maps are separated by newlines
+    let maps_and_seeds = file
+        .lines()
+        .group_by(|&line| line.is_empty());
+
+    let maps_and_seeds = &mut maps_and_seeds
+        .into_iter()
+        .filter(|(is_empty, _)| !is_empty)
+        .map(|(_, lines)| lines);
+
+    // the first line is seeds
+    let seeds = maps_and_seeds
+        .next()
+        .unwrap()
+        .collect::<Vec<&str>>()[0];
+
+    // the first line is the title and can be ignored
+    let maps = maps_and_seeds
+        .map(|mut x| {
+            let _ = x.next();
+
+            x.collect::<Vec<&str>>()
+        })
+        .collect::<Vec<Vec<&str>>>();
+
+    (seeds, maps)
 }
 
 #[cfg(test)]
